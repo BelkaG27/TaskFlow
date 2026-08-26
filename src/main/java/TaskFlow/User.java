5b28e0eb6ccd58e579ecf15591141452c22149fe
Belkacem <belkag7@gmail.com>
2026-08-26 19:41:04 +0200

    created class User

---
+++ src/main/java/TaskFlow/User.java
@@ -0,0 +1,75 @@
+package TaskFlow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message="Le nom d'utilisateur ne peut pas être vide")
+    @Column(unique=true)
+    private String username;
+
+    @NotBlank(message="Le mot de passe ne peut pas être vide")
+    private String password;
+
+    @Email(message="L'adresse e-mail n'est pas valide")
+    private String email;
+
+    @OneToMany(mappedBy="user",cascade=CascadeType.ALL, orphanRemoval=true)
+    private List<Task> tasks = new ArrayList<>();
+
+    public User(){};
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }   
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
